3d10506f76f4615ae9dae6c1a929ad05cbcd3936
Achyuta <achyutathegreat@gmail.com>
2022-08-04 21:41:10 +0530

    7 - Evaluating Expressions

--- java/com/craftinginterpreters/lox/Lox.java
+++ java/com/craftinginterpreters/lox/Lox.java
@@ -9,13 +9,17 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox {
+    private static final Interpreter interpreter = new Interpreter();
+
     static boolean hadError = false;
+    static boolean hadRuntimeError = false;
 
     private static void runFile(String path) throws IOException {
 	byte[] bytes = Files.readAllBytes(Paths.get(path));
 	run(new String(bytes, Charset.defaultCharset()));
 
 	if (hadError) System.exit(65);
+	if (hadRuntimeError) System.exit(70);
     }
 
     private static void runPrompt() throws IOException {
@@ -40,8 +44,7 @@ public class Lox {
 
 	// Stop if there was a syntax error.
 	if (hadError) return;
-
-	System.out.println(new AstPrinter().print(expression));
+	interpreter.interpret(expression);
     }
 
     static void error(int line, String message) {
@@ -54,6 +57,11 @@ public class Lox {
 	} else {
 	    report(token.line, " at '" + token.lexeme + "'", message);
 	}
+    }
+
+    static void runtimeError(RuntimeError error) {
+	System.err.println(error.getMessage() + "\n[line " + error.token.line + "]");
+	hadRuntimeError = true;
     }
 
     private static void report(int line, String where, String message) {
