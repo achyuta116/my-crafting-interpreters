20eb5b9774ec6d5cfbdd0e7c68485c50649c603a
Achyuta <achyutathegreat@gmail.com>
2022-08-09 22:20:30 +0530

    8 - Statements and State

--- java/com/craftinginterpreters/lox/Interpreter.java
+++ java/com/craftinginterpreters/lox/Interpreter.java
@@ -1,17 +1,78 @@
 package com.craftinginterpreters.lox;
 
-class Interpreter implements Expr.Visitor<Object> {
-   
-    void interpret(Expr expression) {
+import java.util.List;
+
+class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
+    
+    private Environment environment = new Environment();
+
+    void interpret(List<Stmt> statements) {
 	try {
-	    Object value = evaluate(expression);
-	    System.out.println(stringify(value));
+	    for (Stmt statement : statements) {
+		execute(statement);
+	    }
 	} catch (RuntimeError error) {
 	    Lox.runtimeError(error);
 	}
     }
+
+    private void execute(Stmt stmt) {
+	stmt.accept(this);
+    }
+
+    void executeBlock(List<Stmt> statements,
+	    Environment enironment) {
+	Environment previous = this.environment;
+	try {
+	    this.environment = environment;
+
+	    for (Stmt statement : statements) {
+		execute(statement);
+	    }
+	} finally {
+	    this.environment = previous;
+	}
+    }
+
+    @Override
+    public Void visitBlockStmt(Stmt.Block stmt) {
+	executeBlock(stmt.statements, new Environment(environment));
+	return null;
+    }
+
     private Object evaluate(Expr expr) {
 	return expr.accept(this);
+    }
+
+    @Override
+    public Void visitExpressionStmt(Stmt.Expression stmt) {
+	evaluate(stmt.expression);
+	return null;
+    }
+
+    @Override
+    public Void visitPrintStmt(Stmt.Print stmt) {
+	Object value = evaluate(stmt.expression);
+	System.out.println(stringify(value));
+	return null;
+    }
+
+    @Override 
+    public Void visitVarStmt(Stmt.Var stmt) {
+	Object value = null;
+	if (stmt.initializer != null) {
+	    value = evaluate(stmt.initializer);
+	}
+
+	environment.define(stmt.name.lexeme, value);
+	return null;
+    }
+
+    @Override
+    public Object visitAssignExpr(Expr.Assign expr) {
+	Object value = evaluate(expr.value);
+	environment.assign(expr.name, value);
+	return value;
     }
 
     @Override
@@ -32,6 +93,11 @@ class Interpreter implements Expr.Visitor<Object> {
 	}
 
 	return null;
+    }
+
+    @Override
+    public Object visitVariableExpr(Expr.Variable expr) {
+	return environment.get(expr.name);
     }
 
     private void checkNumberOperand(Token operator, Object operand) {
@@ -79,13 +145,13 @@ class Interpreter implements Expr.Visitor<Object> {
 	    case BANG_EQUAL: return !isEqual(left, right);
 	    case EQUAL_EQUAL: return isEqual(left, right);
 	    case PLUS:
-		if (left instanceof Double && right instanceof Double) {
-		    return (double)left + (double)right;
-		}
-		if(left instanceof String && right instanceof String) {
-		    return (String)left + (String)right;
-		}
-		throw new RuntimeError(expr.operator, "Operands must be two numbers or two strings.");
+			      if (left instanceof Double && right instanceof Double) {
+				  return (double)left + (double)right;
+			      }
+			      if(left instanceof String && right instanceof String) {
+				  return (String)left + (String)right;
+			      }
+			      throw new RuntimeError(expr.operator, "Operands must be two numbers or two strings.");
 	}
 
 	return null;
--- syntactic_grammar.md
+++ syntactic_grammar.md
@@ -14,11 +14,22 @@ operator    -> "==" | "!=" | "<" | "<=" | ">" | ">="
 	    | "+"  | "-"  | "*" | "/" ;
 
 ##### Precedence for parsing expressions
-expression  -> equality
+expression  -> assignment ;
+assignment  -> IDENTIFIER "=" assignment
+	     | equality ;
 equality    -> comparison ( ( "!=" | "==" ) comparison )\* ;
 comparison  -> term ( ( ">" | ">=" | "<" | "<=" ) term )\* ;
 term	    -> factor ( ( "-" | "+" ) factor )\* ;
 factor	    -> unary ( ( "/" | "\*" ) unary )\* ;
 unary	    -> ("!" | "-") unary | primary ;
-primary	    -> NUMBER | STRING | "true" | "false" | "nil"
+primary	    -> NUMBER | STRING | "true" | "false" | "nil" | IDENTIFIER
 	     | "(" expression ")" ;
+##### Statments
+program	    -> declaration\* EOF ;
+declaration -> varDecl
+               | statement ;
+varDecl        → "var" IDENTIFIER ( "=" expression )? ";" ;
+statement   -> exprStmt
+               | printStmt
+	       | block ;
+block	    -> "{" declaration\* "}"
